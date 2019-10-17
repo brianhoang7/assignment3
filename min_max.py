068d9a75411fd272df5e35839a241e30a2b3f037
brianhoang <hoangbr@oregonstate.edu>
2019-10-16 22:53:19 -0700

    assignment3abc

--- min_max.py
+++ min_max.py
@@ -1,16 +1,13 @@
 # Author: Brian Hoang
 # Date: 10/16/2019
 # Description: Asks the user how many integers they want to enter and outputs the minimum and maximum numbers in that list
-
 print("How many integers would you like to enter?")
 user_input=int(input())                     #user input for how many integers program will enter
 print("Please enter",int(user_input),"integers.")
-
 start=1                             #sets starting tally for loop
 num1=int(input())                   #takes first user integer input
 min=num1                            #sets min as first int as base
 max=num1                            #sets max as first int as base
-
 while(start<user_input):           #keeps loop running until reaches user desired number of integers
     num1=int(input())               #will take in a new integer everytime loop is ran
     if(num1< min):                   #if integer is smaller than previously stored min, it will replace
@@ -18,7 +15,5 @@ while(start<user_input):           #keeps loop running until reaches user desire
     elif(num1>max):
         max=num1
     start+=1                          #adds 1 to start to increase loop tally. once tally reaches user_input, loop will stop
-
-
 print("min: ",int(min))               #prints out min and max
 print("max: ",int(max))
--- num_guess.py
+++ num_guess.py
@@ -1,14 +1,11 @@
 # Author: Brian Hoang
 # Date: 10/16/2019
 # Description: asks user to enter number to be guessed. then asks user/player for a guess at original number and prints out if it is too low, too high, or is correct. then it will print out number of tries it took to guess
-
-
 print("Enter the number for the player to guess: ")
 num1=int(input())                                 #variable for actual number
 print("Enter your guess.")
 guess=int(input())                                #variable for player guess
 start=0                                           #begins tally at 0
-
 while True:
     if(guess==num1):                              #condition for if guess is correct
         start+=1                                    #adds 1 tally
